97608bc10af2fe542c6a9b00a6ddc7d70571a4f5
jflynn129 <jflynn129@gmail.com>
2019-08-28 09:22:53 -0400

    x

---
+++ Nrf52App/pca10040/s132/ses/Output/Release/Exe/combine.bat
@@ -0,0 +1,1 @@
+mergehex -m C:\Users\jflynn129_w10\source\nRF5_SDK\nRF5_SDK_15.2.0_9412b96\components\softdevice\s132\hex\s132_nrf52_6.1.0_softdevice.hex .\ble_app_uart_pca10040_s132.hex -o combined.hex
